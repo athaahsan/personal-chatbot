06fb178ad79876149634b1c7f6402318ef0ef0e8
athaahsan <atha.ahsan.xavier.haris@gmail.com>
2025-09-19 21:01:23 +0700

    image compression...

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -168,6 +168,7 @@ const UserInput = forwardRef(({
                 const options = {
                     maxSizeMB: 1,
                     maxWidthOrHeight: 1280,
+                    initialQuality: 0.9,
                     fileType: 'image/jpeg',
                     useWebWorker: true,
                 };
