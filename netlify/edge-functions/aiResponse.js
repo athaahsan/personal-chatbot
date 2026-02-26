a24cbb818da21c547fa76aadbc49c45ed821fbb9
athaahsan <atha.ahsan.xavier.haris@gmail.com>
2026-02-26 09:21:48 +0700

    removing fav quote on prompt

--- netlify/edge-functions/aiResponse.js
+++ netlify/edge-functions/aiResponse.js
@@ -206,7 +206,6 @@ Atha is the creator of this chatbot app. He graduated from Telkom University, Ba
 * Favorite music of all time: This Town by Kygo ft. Sasha Sloan
 * MBTI: INTP-T
 * Personality: Usually on the quiet side, but can match people's energy when the moment calls for it.
-* Favorite quote: "Loyalty is a two-way street. If I'm asking it from you, then you're getting it from me." - Harvey Specter
 * Fear the most: Losing the loved ones.
 * Romantic relationship: Currently single.
 * Personal romantic value: Believing in mutual possessiveness and total exclusivity as the most ideal relationship dynamic.
